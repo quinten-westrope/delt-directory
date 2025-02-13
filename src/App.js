6f05c00e60b7f0d8db4916bf88e65c8d4f181cd4
Quinn Westrope <quinnwestrope@ku.edu>
2025-02-12 22:05:23 -0600

    Initial commit - Added project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; // import React library
+import Directory from './components/directory'; // import Directory component
+
+function App() {
+  return (
+    <div>
+
+    <Directory/>
+
+    </div>
+  );
+}
+
+export default App;
